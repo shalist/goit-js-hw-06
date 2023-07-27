7bbe11c764e4ade563d44fcfd3c7231773312601
shalist <NE401960@gmail.com>
2023-07-27 15:51:55 +0300

    hw-6_task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,5 @@
-// визначаємо всі "li" з класом "item" в загальному "ul"
 const items = document.querySelectorAll("ul#categories > li.item").length;
-// пребираємо кожен елемент наших елементів за допомогою "ForEach"
 items.forEach((item) => {
-    // створюємо змінну "titleName", яка показує назву секції
     const titleName = item.querySelector("h2").textContent;
-    // створюємо змінну "numberOfElements", яка рахує кількість елементів в секції
     const numberOfElements = item.querySelectorAll("ul li").length;
 })
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul#ingredients");
+
+ingredients.forEach(element => {
+  const li = document.createElement("li");
+  li.classList.add("item");
+  li.textContent = element;
+
+  list.append(li)
+})
