a94aedcfdadf8c2b1fe4fc00d23629dcf3f49b1b
shalist <NE401960@gmail.com>
2023-07-29 20:36:20 +0300

    hw-6_task-9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+const bodyEl = document.body;
+const spnColor = document.querySelector('.color')
+
+
+btnEl.addEventListener('click', handlerBackgroundColor);
+
+function handlerBackgroundColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spnColor.textContent = bodyEl.style.backgroundColor
+}
