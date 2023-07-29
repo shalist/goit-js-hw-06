d71eabbe5b1c5f9372b533c5deb2538dacca37e1
shalist <NE401960@gmail.com>
2023-07-29 18:47:06 +0300

    hw-6_task-8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+
+    if (email.value === '' || password.value === '') {
+        alert('Всі поля повинні бути заповнені!')
+    } else {
+        const objectForConsole = {
+            email: email.value,
+            password: password.value
+        }
+        console.log(objectForConsole);
+    }
+    form.reset();
+}
+
+
+
 
